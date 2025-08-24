5d012624cbeeff9558d64bbb59bf657a7c527a2c
Adebayo Michael <48318606+baytech77@users.noreply.github.com>
2025-08-24 10:53:59 +0100

    Update views.py

--- encoderapp/views.py
+++ encoderapp/views.py
@@ -38,8 +38,8 @@ class EncodeImageView(FormView):
         img_content = ContentFile(img_io.getvalue(), 'encodede_image.png')
         
         # saving to database
-        #encoded_image_instance = EncodedImage(cover_image=form.cleaned_data['cover_image'])
-        #encoded_image_instance.encoded_image.save('encoded_image.png', img_content)
+        encoded_image_instance = EncodedImage(cover_image=form.cleaned_data['cover_image'])
+        encoded_image_instance.encoded_image.save('encoded_image.png', img_content)
         #encoded_image_instance.save()
 
 
